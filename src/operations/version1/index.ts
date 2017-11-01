843d7247576fb11c282e1f5b5cc1de2bda628e10
Sergey Seroukhov <seroukhov@gmail.com>
2017-10-31 17:25:48 -0700

    Added missing exports

--- src/operations/version1/index.ts
+++ src/operations/version1/index.ts
@@ -4,3 +4,5 @@ export { EventLogOperationsV1 } from './EventLogOperationsV1';
 export { SettingsOperationsV1 } from './SettingsOperationsV1';
 export { StatisticsOperationsV1 } from './StatisticsOperationsV1';
 export { BlobsOperationsV1 } from './BlobsOperationsV1';
+export { EmailOperationsV1 } from './EmailOperationsV1';
+export { SmsOperationsV1 } from './SmsOperationsV1';
